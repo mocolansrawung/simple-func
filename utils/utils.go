6205ba73291bc20834550ba5cb9f3b6e78391dc0
Yudho Ahmad Fahreza <yudhoahmadz@gmail.com>
2023-07-26 12:04:34 +0700

    fixing scan error and add append func

--- utils/utils.go
+++ utils/utils.go
@@ -4,29 +4,54 @@ import (
 	"fmt"
 )
 
-func scanInputs(n int, inputType string) {
+func scanInputs(n int, inputType string) (inputInt []int, inputStr []string) {
 	switch inputType {
 	case "int":
-		inputs := make([]int, n)
+		var inputInt []int
 		for i := 0; i < n; i++ {
 			fmt.Printf("Enter input %d (integer): ", i+1)
-			_, err := fmt.Scan(&inputs[i])
+			_, err := fmt.Scan(&inputInt[i])
 			if err != nil {
 				fmt.Println("Error reading input: ", err)
-				return
+				return inputInt, nil
 			}
 		}
 	case "string":
-		inputs := make([]string, n)
+		var inputStr []string
 		for i := 0; i < n; i++ {
 			fmt.Printf("Enter input %d (string): ", i+1)
-			_, err := fmt.Scan(&inputs[i])
+			_, err := fmt.Scan(&inputStr[i])
 			if err != nil {
 				fmt.Println("Error reading input: ", err)
-				return
+				return nil, inputStr
 			}
 		}
 	default:
 		fmt.Println("inputType not supported right now.")
 	}
+
+	return nil, nil
 }
+
+func readAndWriteInputs() string {
+	_, inputStr := scanInputs(2, "string")
+	if inputStr != nil {
+		result := inputStr[0] + inputStr[1]
+		fmt.Println(result)
+		return result
+	}
+	return "Input error."
+}
+
+// func appendAndPrintInputs(a, b string) {
+// 	fmt.Printf("%s %s", a, b)
+// 	return
+// }
+
+// func readIntAndTimes() {
+// }
+
+// // fmt.Println("You entered the following inputs:")
+// // for i, val := range inputs {
+// // 	fmt.Printf("Input %d: %d\n", i+1, val)
+// // }
